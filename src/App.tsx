e9e260c9d03070c56d6a03c605746321f808475d
jjmares <jjmares912@gmail.com>
2025-02-03 22:27:33 -0600

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import { Button } from "react-bootstrap"
+
+function App() {
+
+  return (
+    <div>
+      <Button>Test</Button>
+    </div>
+  )
+}
+export default App
---
+++ src/data.ts
@@ -0,0 +1,34 @@
+import { TodoItem } from "./types";
+
+export const defaultTodos: TodoItem[] = [
+  {
+    id: 1,
+    title: "Learn Typescript",
+    completed: true,
+  },
+  {
+    id: 2,
+    title: "Organize schedule",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Wake up early",
+    completed: true,
+  },
+  {
+    id: 4,
+    title: "Walk my dog",
+    completed: false,
+  },
+  {
+    id: 5,
+    title: "Read a book",
+    completed: true,
+  },
+  {
+    id: 6,
+    title: "Workout",
+    completed: false,
+  },
+];
